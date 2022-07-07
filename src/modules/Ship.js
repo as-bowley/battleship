b4cb6a501df0989a9df328717aff2de4a2a1b33e
as-bowley <as.bowley@gmail.com>
2022-07-07 21:33:43 +0200

    add classes for board states

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,27 +1,24 @@
-const { values } = require("lodash");
+const Ship = (length) => {
+  const shipsHealth = Array(length).fill(0);
 
-const Ship = (location) => {
-  const coordinates = location;
-  const shipsHealth = Array(coordinates.length).fill(0);
-  const getLength = () => {
-    return coordinates.length;
-  };
-  const getCoords = () => {
-    return coordinates;
-  };
   const checkShipHealth = () => {
     return shipsHealth;
   };
-  const hit = (coordinate) => {
-    shipsHealth[coordinates.indexOf(coordinate)] = 1;
+
+  const hit = (index) => {
+    shipsHealth[index] = 1;
+    isSunk();
     return shipsHealth;
   };
+
   const isSunk = () => {
-    if (shipsHealth.reduce((acc, curVal) => acc + curVal) === 3) {
-      return "She's a goner!";
+    if (shipsHealth.reduce((acc, curVal) => acc + curVal) === length) {
+      return true;
+    } else {
+      return false;
     }
   };
-  return { getLength, getCoords, checkShipHealth, hit, isSunk };
+  return { checkShipHealth, hit, isSunk };
 };
 
 module.exports = Ship;
