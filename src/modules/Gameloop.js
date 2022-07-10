14175fa339639a9958384b938c32baad28c1221e
as-bowley <as.bowley@gmail.com>
2022-07-10 19:26:08 +0200

    add placement of ships on click with hover state to show placement

--- src/modules/Gameloop.js
+++ src/modules/Gameloop.js
@@ -15,7 +15,8 @@ const Gameloop = (() => {
   userInterface.renderPlayerGameboard(playerBoard);
   userInterface.renderComputerGameBoard(computerBoard);
 
-  const playerGameBoardCells = document.querySelector(".playerBoardCell");
+  const playerGameBoardCells = document.querySelectorAll(".playerBoardCell");
+  const rotateButton = document.getElementById("rotateButton");
 
   //declare all ships with boolean
   //go through each ship with if statement when using mouseover function or onclick for placeship function e.g. if (carrier === true) {length = 5} & if (carrier === true) {placeShip(carrier....) then carrier = false}
@@ -29,21 +30,141 @@ const Gameloop = (() => {
   let submarine = false;
   let patrolBoat = false;
 
+  let direction = "vertical";
+  rotateButton.addEventListener("click", function () {
+    if (direction == "vertical") {
+      direction = "horizontal";
+    } else {
+      direction = "vertical";
+    }
+  });
+
   const showShipLength = () => {
     if (carrier === false) {
-      return 5;
-    } else if (battleship === false) {
       return 4;
+    } else if (battleship === false) {
+      return 3;
     } else if (destroyer === false) {
-      return 3;
-    } else if (submarine === false) {
-      return 3;
-    } else {
       return 2;
+    } else if (submarine === false) {
+      return 2;
+    } else {
+      return 1;
     }
   };
 
-  playerGameBoardCells.addEventListener("mouseover", showShipLength);
+  const limitCellValue = (num, min = 0, max = 9) => {
+    const parsed = parseInt(num);
+    return Math.min(Math.max(parsed, min), max);
+  };
+
+  const addExtraCells = (cell, direction, length) => {
+    let location = cell.split(/\[(-?\d+)\]/);
+    let row;
+    let col;
+
+    if (direction == "horizontal") {
+      row = location.splice(1, 2).join("");
+      col = Number(location.splice(1, 2).join("")) + 1;
+    } else {
+      row = Number(location.splice(1, 2).join("")) + 1;
+      col = location.splice(1, 2).join("");
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (direction == "horizontal") {
+        const nextCell = document.getElementById(
+          `player: [${row}][${limitCellValue(col + i)}]`
+        );
+        nextCell.classList.add("shipPlacement");
+      } else {
+        const nextCell = document.getElementById(
+          `player: [${limitCellValue(row + i)}][${col}]`
+        );
+        nextCell.classList.add("shipPlacement");
+      }
+    }
+  };
+
+  const removeExtraCells = (cell, direction, length) => {
+    let location = cell.split(/\[(-?\d+)\]/);
+    let row;
+    let col;
+
+    if (direction == "horizontal") {
+      row = location.splice(1, 2).join("");
+      col = Number(location.splice(1, 2).join("")) + 1;
+    } else {
+      row = Number(location.splice(1, 2).join("")) + 1;
+      col = location.splice(1, 2).join("");
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (direction == "horizontal") {
+        const nextCell = document.getElementById(
+          `player: [${row}][${limitCellValue(col + i)}]`
+        );
+        nextCell.classList.remove("shipPlacement");
+      } else {
+        const nextCell = document.getElementById(
+          `player: [${limitCellValue(row + i)}][${col}]`
+        );
+        nextCell.classList.remove("shipPlacement");
+      }
+    }
+  };
+
+  playerGameBoardCells.forEach(function (cell) {
+    cell.addEventListener("mouseover", function (e) {
+      e.target.classList.add("shipPlacement");
+      const cell = e.target.id;
+
+      addExtraCells(cell, direction, showShipLength());
+    });
+    cell.addEventListener("mouseleave", function (e) {
+      e.target.classList.remove("shipPlacement");
+      const cell = e.target.id;
+
+      removeExtraCells(cell, direction, showShipLength());
+    });
+    cell.addEventListener("click", function (e) {
+      const cell = e.target.id;
+      const location = cell.split(/\[(-?\d+)\]/);
+      const row = location.splice(1, 2).join("");
+      const col = location.splice(1, 2).join("");
+      if (carrier === false) {
+        playerBoard.placeShip("carrier", direction, Number(row), Number(col));
+        userInterface.updatePlayerGameboard(playerBoard);
+        carrier = true;
+      } else if (battleship === false) {
+        playerBoard.placeShip(
+          "battleship",
+          direction,
+          Number(row),
+          Number(col)
+        );
+        userInterface.updatePlayerGameboard(playerBoard);
+        battleship = true;
+      } else if (destroyer === false) {
+        playerBoard.placeShip("destroyer", direction, Number(row), Number(col));
+        userInterface.updatePlayerGameboard(playerBoard);
+        destroyer = true;
+      } else if (submarine === false) {
+        playerBoard.placeShip("submarine", direction, Number(row), Number(col));
+        userInterface.updatePlayerGameboard(playerBoard);
+        submarine = true;
+      } else {
+        playerBoard.placeShip(
+          "patrol boat",
+          direction,
+          Number(row),
+          Number(col)
+        );
+        userInterface.updatePlayerGameboard(playerBoard);
+        patrolBoat = true;
+      }
+    });
+  });
 
   //the placement of ships to be determined by drag and drop in final ver.
 
